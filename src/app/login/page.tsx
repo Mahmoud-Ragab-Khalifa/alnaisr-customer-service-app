7da5d89d2ee2b2c475093724a19eb079689ead76
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-20 15:19:49 +0300

    feat: add proxy file to redirect users in app based on auth case

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,11 @@
+import LoginForm from "./_components/LoginForm";
+
 const LoginPage = () => {
-  return <main>LoginPage</main>;
+  return (
+    <main className="min-h-[calc(100dvh-66px)] glass flex items-center justify-center">
+      <LoginForm />
+    </main>
+  );
 };
 
 export default LoginPage;
