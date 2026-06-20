d4245bcbab1dc0f9b40ef8a12c8e9b823605172e
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-20 15:45:48 +0300

    feat: update ui in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Home, Plane, Users } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -9,22 +10,28 @@ const Header = () => {
   return (
     <header className="py-4 glass-strong">
       <div className="container flex items-center justify-between">
-        <Link href={"/"} className="text-xl md:text-2xl">
+        <Link
+          href={"/"}
+          className="text-xl md:text-2xl flex items-center gap-1"
+        >
           مكتب النسر
+          <Plane className="text-orange-400" />
         </Link>
 
         <nav className="flex items-center gap-6">
           <Link
             href={"/"}
-            className={`${pathname === "/" ? "text-primary" : "text-foreground"} text-xl md:text-2xl`}
+            className={`${pathname === "/" ? "text-primary" : "text-foreground"} text-xl md:text-2xl flex items-center gap-1`}
           >
+            <Home className="hidden md:block text-orange-400" />
             الرئيسية
           </Link>
 
           <Link
             href={"/clients"}
-            className={`${pathname === "/clients" ? "text-primary" : "text-foreground"} text-xl md:text-2xl`}
+            className={`${pathname === "/clients" ? "text-primary" : "text-foreground"} text-xl md:text-2xl flex items-center gap-1`}
           >
+            <Users className="hidden md:block text-orange-400" />
             العملاء
           </Link>
         </nav>
