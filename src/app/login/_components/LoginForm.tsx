a143f1a18f431af4d74c1d064331ebc634f6eb6d
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-20 15:35:20 +0300

    refactor: update font in app to be cairo font

--- src/app/login/_components/LoginForm.tsx
+++ src/app/login/_components/LoginForm.tsx
@@ -7,7 +7,7 @@ import { Label } from "@/components/ui/label";
 const LoginForm = () => {
   return (
     <div className="p-4 md:p-0 w-full max-w-md">
-      <form className="flex flex-col gap-6 glass-strong rounded-lg p-6 md:p-8">
+      <form className="flex flex-col gap-6 glass-strong rounded-lg p-6 md:p-8 glow-border">
         <h1 className="text-center md:text-xl font-bold italic glow-text mb-4">
           سجل الدخول الى حسابك لمواصلة العمل
         </h1>
