51c51d4b5096a9346d253f574a4e1f063dc0a0db
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-21 02:47:04 +0300

    feat: add login server action function to use it in client form

---
+++ src/app/login/_actions/auth.ts
@@ -0,0 +1,42 @@
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
+import { loginSchema } from "@/lib/validations/auth";
+
+export const login = async (prevState: unknown, formData: FormData) => {
+  const result = loginSchema.safeParse(Object.fromEntries(formData.entries()));
+
+  if (!result.success) {
+    return {
+      errors: result.error.formErrors.fieldErrors,
+      status: 400,
+      formData,
+    };
+  }
+
+  try {
+    const supabase = await createSupabaseServerClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email: result.data.email,
+      password: result.data.password,
+    });
+
+    if (error) {
+      return {
+        status: error.status,
+        message: error.message,
+      };
+    }
+
+    return {
+      status: 201,
+      message: "تم تسجيل الدخول بنجاح",
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      status: 500,
+      message: "Unexpected Error Check Your Connection",
+    };
+  }
+};
